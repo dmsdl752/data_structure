d67a346d47c6988d207c12593508d51cdcac035f
dmsdl752 <74747353+dmsdl752@users.noreply.github.com>
2021-03-10 16:21:09 +0900

    Add files via upload

---
+++ OneDim.java
@@ -0,0 +1,9 @@
+class OneDim{
+
+public static void main(String srgs[]){
+
+   int sale[] = new int[]{157, 209, 251, 312};
+    for(int i=0; i<4; i++)
+ System.out.printf("%d/4ºÐ±â : sale[%d]= %d %n", i+1, i, sale[i]);
+ }
+}
